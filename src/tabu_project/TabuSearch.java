312b86b3038f7b2dd294069ee6a2565031319489
Grzegorz <grzegorzc66@gmail.com>
2023-01-31 17:21:11 +0100

    tabu withought tabu

---
+++ src/tabu_project/TabuSearch.java
@@ -0,0 +1,61 @@
+package tabu_project;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TabuSearch {
+
+	public static List<Integer> tabu_search(List<Integer> list_of_worker_index, List<List<Integer>> workers_and_tasks) {
+		int MAX_ITER = 1000;
+		List<Integer> worker_index_pair = new ArrayList<>();
+		List<Integer> best_list_of_worker_index_list = new ArrayList<>();
+
+		best_list_of_worker_index_list = new ArrayList<>(list_of_worker_index);
+		for (int i = 0; i < MAX_ITER; i++) {
+			worker_index_pair = generate_pair_for_swap(workers_and_tasks);
+			List<Integer> swaped_list_of_worker_index = swap_workers(list_of_worker_index, worker_index_pair);
+			if (calcualte_total_time(swaped_list_of_worker_index,
+					workers_and_tasks) < calcualte_total_time(list_of_worker_index, workers_and_tasks)) {
+				best_list_of_worker_index_list = new ArrayList<>(swaped_list_of_worker_index);
+			}
+		}
+
+		return best_list_of_worker_index_list;
+	}
+
+	public static List<Integer> swap_workers(List<Integer> list_of_worker_index, List<Integer> worker_pair) {
+		List<Integer> swaped_worker_list = new ArrayList<Integer>(list_of_worker_index);
+
+		swaped_worker_list.set(worker_pair.get(0), worker_pair.get(1));
+		swaped_worker_list.set(worker_pair.get(1), worker_pair.get(0));
+
+		return swaped_worker_list;
+	}
+
+	public static List<Integer> generate_pair_for_swap(List<List<Integer>> workers_and_tasks) {
+
+		Random random = new Random();
+
+		int randomly_generated_first_worker_index = random.nextInt(workers_and_tasks.size());
+		int randomly_generated_second_worker_index = random.nextInt(workers_and_tasks.size());
+
+		while (randomly_generated_first_worker_index == randomly_generated_second_worker_index) {
+			randomly_generated_second_worker_index = random.nextInt(workers_and_tasks.size());
+		}
+
+		// musimy sprawdzić czy jest na liście tabu
+
+		return null;
+	}
+
+	public static int calcualte_total_time(List<Integer> list_of_worker_index, List<List<Integer>> workers_and_tasks) {
+		int sum = 0;
+		for (int i = 0; i < list_of_worker_index.size(); i++) {
+			sum = sum + workers_and_tasks.get(list_of_worker_index.get(i)).get(i);
+		}
+
+		return sum;
+	}
+}
--- src/tabu_project/main.java
+++ src/tabu_project/main.java
@@ -1,6 +1,7 @@
 package tabu_project;
 
 import java.util.List;
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -12,16 +13,48 @@ public class main {
 		List<String> fileContent = BufferedFileReader.readFile();
 		List<String> rawData = BufferedFileReader.parseFileContentLineIntoList(fileContent);
 		List<List<Integer>> data = BufferedFileReader.prepare_list_of_integers(rawData);
+
+		//System.out.println(data.size());
+		//System.out.println(data.get(0).size());
+
+		// pierwszy jest indeks pracownika a drugi index zadania
+		// pionowo pracownik
+		// poziomo jest zadanie
+
+		//List<Integer> first_worker_index_combination = generate_first_combination(data);
+		// print_list_of_numbers(first_worker_index_combination);
+
+		//System.out.println(calcualte_total_cost(first_worker_index_combination, data));
+		List<Integer> randomly_generated_worker_list = assign_randomly_withought_repetitions(data);
+		System.out.println(TabuSearch.calcualte_total_time(randomly_generated_worker_list, data));
+		System.out.println("");
 		
+		print_list_of_numbers(randomly_generated_worker_list);
+		System.out.println("");
+		System.out.println(randomly_generated_worker_list.size());
 		
-		List<Integer> first_combination = generate_first_combination(data);
-		//i pracownik
-		//j zadanie
-		
-		// 
-		
+		List<Integer> result = TabuSearch.tabu_search(randomly_generated_worker_list, data);
 		
 	}
+	
+
+	public static List<Integer> assign_randomly_withought_repetitions(List<List<Integer>> data) {
+		List<Integer> list_of_worker_index = new ArrayList<>();
+
+		for (int task_index = 0; task_index < data.get(0).size(); task_index++) {
+
+			Random random = new Random();
+
+			int randomly_generated_worker_index = random.nextInt(data.size());
+
+			while (is_on_worker_list(randomly_generated_worker_index, list_of_worker_index)) {
+				randomly_generated_worker_index = random.nextInt(data.size());
+			}
+			list_of_worker_index.add(randomly_generated_worker_index);
+		}
+		return list_of_worker_index;
+	}
+
 	public static List<Integer> generate_first_combination(List<List<Integer>> data) {
 		List<Integer> list_of_worker_index = new ArrayList<>();
 
@@ -32,13 +65,24 @@ public class main {
 			for (int worker_index = 0; worker_index < data.size(); worker_index++) {
 				int current_cost_performing_task = data.get(worker_index).get(task_index);
 
+				if ((current_cost_performing_task < cost_of_performing_the_task)
+						&& (!is_on_worker_list(worker_index, list_of_worker_index))) {
+					most_eficient_worker_index = worker_index;
+					cost_of_performing_the_task = current_cost_performing_task;
+				}
 
 			}
 			list_of_worker_index.add(most_eficient_worker_index);
 		}
 		return list_of_worker_index;
 	}
+
+	public static boolean is_on_worker_list(int worker_index, List<Integer> list_of_worker_index) {
+		return list_of_worker_index.contains(worker_index);
+	}
 	
+
+
 	public static void print_list_of_numbers(List<Integer> list_of_numbers) {
 		for (int i = 0; i < list_of_numbers.size(); i++) {
 			if (i % 10 == 0) {
